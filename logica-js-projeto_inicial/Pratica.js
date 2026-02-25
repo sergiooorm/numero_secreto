be36dec349393da30425435b7697f977abb310e5
Sergio Macedo <ribeiro587@hotmail.com>
2026-02-25 20:23:23 -0300

    projeto inicial

---
+++ logica-js-projeto_inicial/Pratica.js
@@ -0,0 +1,22 @@
+let pergunta = prompt('Digite um dia da semana: ');
+let sabado = 'Sabado';
+let domingo = 'Domingo';
+alert(`Voce digitou ${pergunta}`);
+
+if(pergunta == sabado)  {
+    alert ('Bom fim de semana');
+}
+
+if(pergunta == domingo){
+    alert ('Bom fim de semana');
+}
+
+else {
+    alert('Boa semana');
+}
+
+
+
+
+
+
---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,38 @@
+alert('Boas vinda ao jogo do numero secreto !!');
+
+let numeroMaximo = 5000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+
+let chute
+
+let tentativas = 1
+
+console.log('Numero secreto: ', numeroSecreto);
+
+// Enquanto o chute não for igual ao numero secreto
+// !=  diferente de
+while (chute != numeroSecreto) {
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}.`);
+
+    if (numeroSecreto == chute) {
+        break; // se acertar de primeira.
+    } else{ 
+        if (numeroSecreto > chute){
+            alert(`O numero secreto é maior que ${chute}.`);
+        } else {
+            alert(`O numero secreto é menor que ${chute}.`);
+        }
+
+    }
+
+    tentativas ++ ;
+    
+}
+
+// se acertar de primeira, tentativa fica no singular.
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+
+alert(`Isso ai! Voce descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.` );
+
+
+
